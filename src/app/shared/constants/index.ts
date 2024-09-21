59b9a5e42449c629ca8b60492e6a11fc9ffdf572
mendexco <vitormendesco.js@gmail.com>
2024-09-21 14:02:14 -0300

    fix(constants): updating repo link

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -1,5 +1,5 @@
 export const SOCIAL_DATA = [
-  { title: 'Repo', link: 'https://angular.dev' },
+  { title: 'Repo', link: 'https://github.com/mendexco/baby-steps-angular' },
   { title: 'Github', link: 'https://github.com/mendexco' },
   { title: 'LinkedIn', link: 'https://www.linkedin.com/in/vitor-mendesco/' },
 ];
